4ddcf0c217d6dae9357843ba2ca5a0e0eb923b1f
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2021-02-08 19:03:19 +1000

    Added Conversion to XINPUT_STATE

--- hitboard/pipeline/KeyState.cs
+++ hitboard/pipeline/KeyState.cs
@@ -13,16 +13,56 @@ namespace hitboard.pipeline
         DOWN,
         LEFT,
         RIGHT,
+        START,
+        BACK,
+        LEFT_THUMB,
+        RIGHT_THUMB,
         X,
         Y,
         A,
         B,
+        KEYCOUNT
         
     }
+
+
 
     // Represents the current configuration of keys
     class KeyState
     {
 
+        // Look up for Key to XINPUT bitmask
+        public static SortedDictionary<Key, ushort> KEY_TO_MASK =  new  SortedDictionary<Key, ushort>()  {
+                { Key.UP, 0x0001 },
+                { Key.DOWN, 0x0002 },
+                { Key.LEFT, 0x0004 },
+                { Key.RIGHT, 0x0008 },
+                { Key.START, 0x0010 },
+                { Key.BACK, 0x0020 },
+                { Key.LEFT_THUMB, 0x0040 },
+                { Key.RIGHT_THUMB, 0x0080 },
+                { Key.X, 0x4000 },
+                { Key.Y, 0x8000 },
+                { Key.A, 0x1000 },
+                { Key.B, 0x2000 },
+            };
+
+
+        // Current configuration of this KeyState
+        public bool[] Buttons = new bool[(int)Key.KEYCOUNT];
+
+        // Create a new XInputState given this KeyState
+        public XInputState ToXinputState()
+        {
+            XInputState state = new XInputState();
+            foreach (KeyValuePair<Key, ushort> entry in KEY_TO_MASK)
+            {
+                state.Gamepad.Buttons |= (ushort)((Buttons[(int)entry.Key] ? 1 : 0) * KEY_TO_MASK[entry.Key]); 
+            }
+
+            return state;
+        }
     }
+
+
 }
