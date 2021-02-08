3e94e931eb8713919f0ed99a9a3379235c9b0da2
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2021-02-08 19:05:51 +1000

    Marshalled Struct to Alias XINPUT_STATE

---
+++ hitboard/pipeline/XInputState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hitboard.pipeline
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct XInputStateGamepad
+    {
+        public ushort Buttons;
+        public byte LeftTrigger;
+        public byte RightTrigger;
+        public short ThumbLX;
+        public short ThumbLY;
+        public short ThumbRX;
+        public short ThumbRY;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct XInputState
+    {
+        public uint PacketNumber;
+        public XInputStateGamepad Gamepad;
+    }
+}
