61533e2d88790a156111176836d4ec0e8338d6c3
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2021-02-15 13:51:30 +1000

    Added input box

---
+++ hitboard/InputBox.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using hitboard.callback;
+
+namespace hitboard
+{
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox : UserControl
+    {
+        private string _KeyName = "<unassigned>";
+
+        // Transfers _KeyName to label
+        private void DrawKeyLabel()
+        {
+            Key.Content = _KeyName;
+        }
+
+        public string KeyName
+        {
+            get
+            {
+                return _KeyName;
+            }
+            set
+            {
+                _KeyName = value;
+                DrawKeyLabel();
+            }
+        }
+
+        public InputBox()
+        {
+            InitializeComponent();
+            DrawKeyLabel();
+        }
+
+        void Key_Click(object sender, RoutedEventArgs e)
+        {
+            KeyboardPrompt prompt = new KeyboardPrompt();
+            Button.Content = AsciiConverter.KeycodeToAscii(prompt.Resolve());
+        }
+    }
+}
---
+++ hitboard/callback/AsciiConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace hitboard.callback
+{
+    class AsciiConverter
+    {
+        [DllImport("user32.dll")]
+        private static extern uint MapVirtualKeyW(
+          uint uCode,
+          uint uMapType
+        );
+
+        [DllImport("user32.dll")]
+        private static extern int GetKeyNameTextW(
+            int lParam,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeConst = 64)]
+            StringBuilder receivingBuffer,
+            int cchSize
+        );
+
+        public static string KeycodeToAscii(int key)
+        {
+            var buffer = new StringBuilder(1024);
+
+            uint scanCode = MapVirtualKeyW((uint)key, 0);
+            GetKeyNameTextW((int)(scanCode << 16), buffer, 1024);
+
+            return buffer.ToString();
+        }
+    }
+}
